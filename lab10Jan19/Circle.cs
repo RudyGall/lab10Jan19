97728a517711f5b7df704bbb881f10266eb02f10
Rudy Gall <rudyegall@gmail.com>
2019-01-24 15:34:47 -0500

    Initial Commit

---
+++ lab10Jan19/Circle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab10Jan19
+{
+    class Circle
+    {
+        private double radius;
+        public double Radius
+        {
+            get
+            {
+                return radius;
+            }
+            set
+            {
+                radius = value;
+            }
+        }
+        public const double Pi = Math.PI;
+        public Circle(double radius)
+        {           
+        }
+        public double CalculateCircumference()
+        {
+            double circumference = 2 * Pi * radius;
+            return circumference;
+        }
+        public string CalculateFormattedCircumference()
+        {
+            double formattedCircumference = Math.Round(CalculateCircumference(), 2);
+            string FC = formattedCircumference.ToString();
+            return FC;
+        }
+        public double CalculateArea()
+        {
+            double area = Pi * radius * radius;
+            return area;
+        }
+        public string CalculateFormattedArea()
+        {
+            double formattedArea = Math.Round(CalculateArea(), 2);
+            string FA = formattedArea.ToString();
+            return FA;
+        }
+        private string FormatNumber(double x)
+        {
+            string formatNum = Convert.ToString(x);
+            return formatNum;
+        }
+    }
+}
---
+++ lab10Jan19/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab10Jan19
+{
+    class Program
+    {
+        public static double radius { get; private set; }
+        static void Main(string[] args)
+        {
+            int amountOfCircles = 0;
+            bool runP = true;
+            while (runP == true)
+            {            
+                Console.WriteLine("\nPlease enter the radius of a circle");
+                string input = Console.ReadLine();
+                Circle userCircle = new Circle(radius);
+                userCircle.Radius = double.Parse(input);
+                Console.WriteLine("\nThe area of your circle is " + userCircle.CalculateFormattedArea());
+                Console.WriteLine("\nThe circumference of your circle is " + userCircle.CalculateFormattedCircumference());
+                amountOfCircles++;
+                runP = Continue(amountOfCircles);
+            }          
+        }
+        public static bool Continue(int amountOfCircles)
+        {
+            Console.WriteLine("\nWould you like to run the application again? (Y or N)");
+            string input = Console.ReadLine().ToLower();
+            input = input.ToLower();
+            bool goOn;
+            if (input == "y")
+            {
+                goOn = true;               
+            }
+            else if (input == "n")
+            {
+                Console.WriteLine("You created {0} circles, Thank you, Goodbye!", amountOfCircles);
+                Console.ReadLine();
+                goOn = false;
+            }
+            else
+            {
+                Console.WriteLine("I don't understand that, let's try again");
+                goOn = Continue(amountOfCircles);
+            }
+            return goOn;
+        }
+    }
+}
